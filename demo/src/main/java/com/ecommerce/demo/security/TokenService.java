5e4c6897fc6365a55b13ca30d7901a10e09a78e0
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-11-11 18:47:04 -0300

    Adicionado token JWT e criado serviço de geração do JWT

---
+++ demo/src/main/java/com/ecommerce/demo/security/TokenService.java
@@ -0,0 +1,49 @@
+package com.ecommerce.demo.security;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.ecommerce.demo.domain.User;
+
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("Trabalho-final-pw2")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(expireDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Error creating JWT token", e);
+        }
+    }
+    
+    public String getSubject(String tokenJwt) {
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("Trabalho-final-pw2")
+                    .build()
+                    .verify(tokenJwt)
+                    .getSubject();
+        } catch (JWTVerificationException e) {
+            return null;
+        }
+    }
+
+    private Instant expireDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
