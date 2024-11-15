a5be7953f006f005ba79e40a0653a53fd9aba155
juliawel <julia20.wel@hotmail.com>
2024-11-15 18:05:08 -0300

    order controller

--- demo/src/main/java/com/ecommerce/demo/controllers/OrderController.java
+++ demo/src/main/java/com/ecommerce/demo/controllers/OrderController.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -35,8 +37,10 @@ class OrderController {
     }
 
     public ResponseEntity<OrderDTO> insert(@Valid @RequestBody OrderDTO dto) {
-    
-        return null;
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 
 }
