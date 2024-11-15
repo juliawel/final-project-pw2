10630fe83c040c7bd1e36e3b3b03a902ec8b002f
juliawel <julia20.wel@hotmail.com>
2024-11-15 18:15:30 -0300

    product controller

--- demo/src/main/java/com/ecommerce/demo/controllers/ProductController.java
+++ demo/src/main/java/com/ecommerce/demo/controllers/ProductController.java
@@ -3,11 +3,15 @@ package com.ecommerce.demo.controllers;
 import com.ecommerce.demo.dtos.ProductDTO;
 import com.ecommerce.demo.repositories.ProductRepository;
 import com.ecommerce.demo.services.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,14 +26,16 @@ public class ProductController {
         return ResponseEntity.ok(dto);
     }
 
-    public ResponseEntity<ProductDTO> findById(@PathVariable String id) {
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
         ProductDTO dto = service.getById(id);
         return ResponseEntity.ok(dto);
     }
 
-    public ResponseEntity<ProductDTO> insert() {
-
-        return null;
+    public ResponseEntity<ProductDTO> insert(@Valid @PathVariable ProductDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 
     public ResponseEntity<ProductDTO> update() {
